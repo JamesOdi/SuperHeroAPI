adc2dbb8ccc5ef9247dd6d3f683c190ae24f1e46
James Odike <Jamesodike26@gmail.com>
2022-04-02 22:27:23 +0100

    Add project files.

---
+++ SuperHeroAPI/Models/SuperHero.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuperHeroAPI.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
